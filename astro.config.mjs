7d1743c98df69b27484586ab65d297d2c68ad5f2
Cody Barr <cody.barr@gmail.com>
2026-06-11 12:53:18 -0700

    chore: fix cloudflare?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, sessionDrivers } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare({
+    imageService: 'compile'
+  }),
+  session: {
+    driver: sessionDrivers.lruCache()
+  }
 });
